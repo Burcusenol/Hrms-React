2369b46cfd266bf37a65afe1d095c51ff6ff3d75
Burcusenol <59020581+Burcusenol@users.noreply.github.com>
2021-06-13 02:57:11 +0300

    13.06.2021

--- src/layouts/Frame.jsx
+++ src/layouts/Frame.jsx
@@ -1,34 +1,88 @@
 import React from "react";
-import { Container, Header } from "semantic-ui-react";
-
+import {
+  Container,
+  Header,
+  Segment,
+  Grid,
+  Icon,
+  Button,
+  Input,
+} from "semantic-ui-react";
 
 export default function Frame() {
-
-   
   return (
     <div>
       <Container>
         <Header
-         inverted
+          inverted
           as="h1"
-          content="Imagine-a-Company"
+          content="FIND JOBS, CREATE TRACKABLE RESUMES AND ENRICH YOUR APPLICATIONS."
           style={{
-            fontSize: "4em",
+            fontSize: "1.6em",
             fontWeight: "normal",
             marginBottom: 0,
-            marginTop:  "3em",
+            marginTop: "1.5em",
           }}
         />
         <Header
           as="h2"
-          content="Do whatever you want when you want to."
+          content="The Easiest Way to Get Your New Job"
           inverted
           style={{
-            fontSize: "1.7em",
+            fontSize: "4em",
             fontWeight: "normal",
-            marginTop: "1.5em",
+            marginTop: "1.1em",
           }}
         />
+        <Segment inverted color="grey">
+          <Segment inverted color="grey">
+            <Grid columns="equal" textAlign="center">
+              <Grid.Row verticalAlign="middle">
+                <Grid.Column>
+                  <Input
+                    icon="search"
+                    iconPosition="left"
+                    placeholder="Job Title..."
+                  />
+                </Grid.Column>
+                <Grid.Column>
+                  <Input
+                    list="city"
+                    icon="location arrow"
+                    iconPosition="left"
+                    placeholder="Location..."
+                  />
+                  <datalist id="city">
+                    <option value="English">English</option>
+                    <option value="Chinese">Chinese</option>
+                    <option value="Dutch">Dutch</option>
+                  </datalist>
+                </Grid.Column>
+                <Grid.Column>
+                  <Input
+                    list="city"
+                    icon="list alternate"
+                    iconPosition="left"
+                    placeholder="Categories..."
+                  />
+                  <datalist id="city">
+                    <option value="English">English</option>
+                    <option value="Chinese">Chinese</option>
+                    <option value="Dutch">Dutch</option>
+                  </datalist>
+                </Grid.Column>
+                <Grid.Column>
+                  <Button animated basic inverted color="violet" size="massive">
+                    <Button.Content visible>Submit</Button.Content>
+                    <Button.Content hidden>
+                      <Icon name="arrow right" />
+                    </Button.Content>
+                  </Button>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Segment>
+        </Segment>
       </Container>
     </div>
   );
