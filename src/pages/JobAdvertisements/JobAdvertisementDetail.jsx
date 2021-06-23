6ab7c0467c66919cb513720f6d2206dc49ef5de2
Burcusenol <59020581+Burcusenol@users.noreply.github.com>
2021-06-24 01:32:17 +0300

    24.06.2021
    Code refactoring.

--- src/pages/JobAdvertisements/JobAdvertisementDetail.jsx
+++ src/pages/JobAdvertisements/JobAdvertisementDetail.jsx
@@ -1,41 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router";
-import {
-  Container,
-  Segment,
-  Grid,
-  Card,
-  Icon,
-  Divider,
-  Label,
-} from "semantic-ui-react";
+import { useParams } from "react-router-dom";
+import { Container, Segment, Grid, Card,Icon} from "semantic-ui-react";
 import JobAdvertisementService from "../../services/jobAdvertisementService";
 
 export default function JobAdvertisementDetails() {
-
   let { id } = useParams();
   const [jobPost, setJobPost] = useState({});
 
   useEffect(() => {
     let jobAdvertisementService = new JobAdvertisementService();
-    jobAdvertisementService .getByJobAdvertisementId(id).then((result) => setJobPost(result.data.data))
-  }, []);
+    jobAdvertisementService.getByJobAdvertisementId(id).then((result) => setJobPost(result.data));
+  }, [id]);
 
   return (
     <div>
       <Segment circle="true" style={{ padding: "27em 0em" }} vertical>
-        <Container>
-        
-
-        
+      <Container>
           <Grid celled>
             <Grid.Row>
               <Grid.Column width={11}>
                 <Card fluid color="green">
                   <Card.Content>
-                    <Card.Header></Card.Header>
-                    <Card.Meta></Card.Meta>
-
+                    <Card.Header textAlign="left" style={{fontSize:"2em",color:"purple"}}>{jobPost.jobTitle?.title}</Card.Header>
+                    <Card.Meta textAlign="left" style={{fontSize:"1.5em",color:"grey",marginTop:"1em"}}><Icon name="building outline" color="brown"/> {jobPost.employer?.companyName}</Card.Meta>
+                    <Card.Meta textAlign="left" style={{fontSize:"1.5em",color:"grey",marginTop:"0.50em"}}><Icon name="map marker" color="blue"/> {jobPost.city?.cityName}</Card.Meta>
                     <Card.Description></Card.Description>
                   </Card.Content>
                 </Card>
@@ -52,7 +40,7 @@ export default function JobAdvertisementDetails() {
                 </Card>
               </Grid.Column>
             </Grid.Row>
-          </Grid>  
+          </Grid>
         </Container>
       </Segment>
     </div>
